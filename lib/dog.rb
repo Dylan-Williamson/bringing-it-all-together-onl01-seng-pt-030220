33e3ed3b739f8f9d57875653a8348020b194f33c
Dylan Williamson <dirtyassweed@icloud.com>
2020-06-01 22:18:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,7 @@ class Dog
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
   end
@@ -53,7 +53,7 @@ class Dog
       WHERE id = ?
       LIMIT 1
     SQL
-    DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute(sql,id).map do |row|
       self.new_from_db(row)
     end.first
   end
